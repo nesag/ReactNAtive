9b87fc2a0bc265ca682eced97ceaa156c4479916
Ismael Aguilar <ismael@gluo.mx>
2018-11-02 21:45:45 -0600

    agregando funcionalidad test

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PresentationalComponent from './PresentationalComponent'
 
 export default class App extends React.Component {
+
+    state ={
+        myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed ' +
+        '         do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+        '         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+        '         ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ' +
+        '         in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' +
+        '         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia' +
+        '         deserunt mollit anim id est laborum.'
+
+    } ;
+
+    updateState = () => {
+        this.setState({ myState: 'The state is updated' })
+    };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+          <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState}/>
       </View>
     );
   }
